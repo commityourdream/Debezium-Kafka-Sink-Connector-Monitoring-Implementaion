0243d24a186fe0065900b0230e97c3bd463d7ca4
Amit Kumar Manjhi <42512407+commityourdream@users.noreply.github.com>
2023-06-26 15:32:05 +0530

    commenting shell script

--- set_debezium_version.sh
+++ set_debezium_version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set the DEBEZIUM_VERSION environment variable
-set DEBEZIUM_VERSION=2.0
+#set DEBEZIUM_VERSION=2.0
 
 
 # Execute the set_debezium_version.sh script
-./set_debezium_version.sh
+#./set_debezium_version.sh
 
 
